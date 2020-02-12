7fccd0ee45b1051191a2303e4638728a3eba6379
Rodrigossff91 <rodrigossff91@gmail.com>
2020-02-12 18:17:46 -0300

    oi

--- uber/lib/telas/PainelPassageiro.dart
+++ uber/lib/telas/PainelPassageiro.dart
@@ -18,157 +18,197 @@ class PainelPassageiro extends StatefulWidget {
 }
 
 class _PainelPassageiroState extends State<PainelPassageiro> {
-  TextEditingController _controllerDestino =
-      TextEditingController(text: "av. Maria Condexa, 600");
-  List<String> itensMenu = ["Configurações", "Deslogar"];
+
+  TextEditingController _controllerDestino = TextEditingController(text: "av. paulista, 807" );
+  List<String> itensMenu = [
+    "Configurações", "Deslogar"
+  ];
   Completer<GoogleMapController> _controller = Completer();
-  CameraPosition _posicaoCamera =
-      CameraPosition(target: LatLng(-23.563999, -46.653256));
+  CameraPosition _posicaoCamera = CameraPosition(
+      target: LatLng(-23.563999, -46.653256)
+  );
   Set<Marker> _marcadores = {};
 
-  //Controles para exibir na tela
+  //Controles para exibição na tela
   bool _exibirCaixaEnderecoDestino = true;
-  String _textoBotao = "Chamar Uber";
-  Color _colorBotao = Color(0xff1ebbd8);
+  String _textoBotao = "Chamar uber";
+  Color _corBotao = Color(0xff1ebbd8);
   Function _funcaoBotao;
 
   _deslogarUsuario() async {
+
     FirebaseAuth auth = FirebaseAuth.instance;
 
     await auth.signOut();
     Navigator.pushReplacementNamed(context, "/");
+
   }
 
-  _escolhaMenuItem(String escolha) {
-    switch (escolha) {
-      case "Deslogar":
+  _escolhaMenuItem( String escolha ){
+
+    switch( escolha ){
+      case "Deslogar" :
         _deslogarUsuario();
         break;
-      case "Configurações":
+      case "Configurações" :
+
         break;
     }
+
   }
 
-  _onMapCreated(GoogleMapController controller) {
-    _controller.complete(controller);
+  _onMapCreated( GoogleMapController controller ){
+    _controller.complete( controller );
   }
 
-  _adicionarListenerLocalizacao() {
+  _adicionarListenerLocalizacao(){
+
     var geolocator = Geolocator();
-    var locationOptions =
-        LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 10);
+    var locationOptions = LocationOptions(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 10
+    );
 
-    geolocator.getPositionStream(locationOptions).listen((Position position) {
-      _exibirMarcadorPassageiro(position);
+    geolocator.getPositionStream( locationOptions ).listen((Position position){
+
+      _exibirMarcadorPassageiro( position );
 
       _posicaoCamera = CameraPosition(
-          target: LatLng(position.latitude, position.longitude), zoom: 19);
+          target: LatLng(position.latitude, position.longitude),
+          zoom: 19
+      );
 
-      _movimentarCamera(_posicaoCamera);
+      _movimentarCamera( _posicaoCamera );
+
     });
+
   }
 
   _recuperaUltimaLocalizacaoConhecida() async {
+
     Position position = await Geolocator()
-        .getLastKnownPosition(desiredAccuracy: LocationAccuracy.high);
+        .getLastKnownPosition( desiredAccuracy: LocationAccuracy.high );
 
     setState(() {
-      if (position != null) {
-        _exibirMarcadorPassageiro(position);
+      if( position != null ){
+
+        _exibirMarcadorPassageiro( position );
 
         _posicaoCamera = CameraPosition(
-            target: LatLng(position.latitude, position.longitude), zoom: 19);
+            target: LatLng(position.latitude, position.longitude),
+          zoom: 19
+        );
 
-        _movimentarCamera(_posicaoCamera);
+        _movimentarCamera( _posicaoCamera );
+
       }
     });
+
   }
 
-  _movimentarCamera(CameraPosition cameraPosition) async {
+  _movimentarCamera( CameraPosition cameraPosition ) async {
+
     GoogleMapController googleMapController = await _controller.future;
-    googleMapController
-        .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+    googleMapController.animateCamera(
+      CameraUpdate.newCameraPosition(
+        cameraPosition
+      )
+    );
+
   }
 
   _exibirMarcadorPassageiro(Position local) async {
+
     double pixelRatio = MediaQuery.of(context).devicePixelRatio;
 
     BitmapDescriptor.fromAssetImage(
-            ImageConfiguration(devicePixelRatio: pixelRatio),
-            "imagens/passageiro.png")
-        .then((BitmapDescriptor icone) {
+        ImageConfiguration(devicePixelRatio: pixelRatio),
+        "imagens/passageiro.png"
+    ).then((BitmapDescriptor icone){
+
       Marker marcadorPassageiro = Marker(
           markerId: MarkerId("marcador-passageiro"),
           position: LatLng(local.latitude, local.longitude),
-          infoWindow: InfoWindow(title: "Meu local"),
-          icon: icone);
+          infoWindow: InfoWindow(
+              title: "Meu local"
+          ),
+          icon: icone
+      );
 
       setState(() {
-        _marcadores.add(marcadorPassageiro);
+        _marcadores.add( marcadorPassageiro );
       });
+
     });
+
+
+
   }
 
   _chamarUber() async {
+
     String enderecoDestino = _controllerDestino.text;
 
-    if (enderecoDestino.isNotEmpty) {
-      List<Placemark> listaEnderecos =
-          await Geolocator().placemarkFromAddress(enderecoDestino);
+    if( enderecoDestino.isNotEmpty ){
 
-      if (listaEnderecos != null && listaEnderecos.length > 0) {
-        Placemark endereco = listaEnderecos[0];
-        Destino destino = Destino();
-        destino.cidade = endereco.administrativeArea;
-        destino.cep = endereco.postalCode;
-        destino.bairro = endereco.subLocality;
-        destino.rua = endereco.thoroughfare;
-        destino.numero = endereco.subThoroughfare;
+     List<Placemark> listaEnderecos = await Geolocator()
+         .placemarkFromAddress( enderecoDestino );
 
-        destino.latitude = endereco.position.latitude;
-        destino.longitude = endereco.position.longitude;
+     if( listaEnderecos != null && listaEnderecos.length > 0 ){
 
-        String enderecoConfirmacao;
-        enderecoConfirmacao = "\n Cidade: " + destino.cidade;
-        enderecoConfirmacao += "\n Rua: " + destino.rua + ", " + destino.numero;
-        enderecoConfirmacao += "\n Bairro: " + destino.bairro;
-        enderecoConfirmacao += "\n Cep: " + destino.cep;
+       Placemark endereco = listaEnderecos[0];
+       Destino destino = Destino();
+       destino.cidade = endereco.administrativeArea;
+       destino.cep = endereco.postalCode;
+       destino.bairro = endereco.subLocality;
+       destino.rua = endereco.thoroughfare;
+       destino.numero = endereco.subThoroughfare;
 
-        showDialog(
-            context: context,
-            builder: (contex) {
-              return AlertDialog(
-                title: Text("Confirmação do endereço"),
-                content: Text(enderecoConfirmacao),
-                contentPadding: EdgeInsets.all(16),
-                actions: <Widget>[
-                  FlatButton(
-                    child: Text(
-                      "Cancelar",
-                      style: TextStyle(color: Colors.red),
-                    ),
-                    onPressed: () => Navigator.pop(contex),
-                  ),
-                  FlatButton(
-                    child: Text(
-                      "Confirmar",
-                      style: TextStyle(color: Colors.green),
-                    ),
-                    onPressed: () {
-                      //salvar requisicao
-                      _salvarRequisicao(destino);
+       destino.latitude = endereco.position.latitude;
+       destino.longitude = endereco.position.longitude;
 
-                      Navigator.pop(contex);
-                    },
-                  )
-                ],
-              );
-            });
-      }
+       String enderecoConfirmacao;
+       enderecoConfirmacao = "\n Cidade: " + destino.cidade;
+       enderecoConfirmacao += "\n Rua: " + destino.rua + ", " + destino.numero ;
+       enderecoConfirmacao += "\n Bairro: " + destino.bairro ;
+       enderecoConfirmacao += "\n Cep: " + destino.cep ;
+
+       showDialog(
+           context: context,
+         builder: (contex){
+             return AlertDialog(
+               title: Text("Confirmação do endereço"),
+               content: Text(enderecoConfirmacao),
+               contentPadding: EdgeInsets.all(16),
+               actions: <Widget>[
+                 FlatButton(
+                   child: Text("Cancelar", style: TextStyle(color: Colors.red),),
+                   onPressed: () => Navigator.pop(contex),
+                 ),
+                 FlatButton(
+                   child: Text("Confirmar", style: TextStyle(color: Colors.green),),
+                   onPressed: (){
+
+                     //salvar requisicao
+                     _salvarRequisicao( destino );
+
+                     Navigator.pop(contex);
+
+                   },
+                 )
+               ],
+             );
+         }
+       );
+
+     }
+
     }
+
   }
 
-  _salvarRequisicao(Destino destino) async {
+  _salvarRequisicao( Destino destino ) async {
+
     /*
 
     + requisicao
@@ -189,28 +229,54 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
 
     Firestore db = Firestore.instance;
 
-    db.collection("requisicoes").add(requisicao.toMap());
+    db.collection("requisicoes")
+    .add( requisicao.toMap() );
+
+    _statusAguardando();
+
+
   }
 
-  _alterarBotaoPrincipal(String texto, Color cor,Function funcao){
+  _alterarBotaoPrincipal(String texto, Color cor, Function funcao){
 
     setState(() {
       _textoBotao = texto;
-      _colorBotao = cor;
+      _corBotao = cor;
       _funcaoBotao = funcao;
     });
+
   }
 
   _statusUberNaoChamado(){
 
     _exibirCaixaEnderecoDestino = true;
+
     _alterarBotaoPrincipal(
-      "Chamar Uber", 
-       Color(0xff1ebbd8),
-       (){
-         _chamarUber();
-       }
+        "Chamar uber",
+        Color(0xff1ebbd8),
+        (){
+          _chamarUber();
+        }
     );
+
+  }
+
+  _statusAguardando(){
+
+    _exibirCaixaEnderecoDestino = false;
+
+    _alterarBotaoPrincipal(
+        "Cancelar",
+        Colors.red,
+            (){
+          _cancelarUber();
+        }
+    );
+
+  }
+
+  _cancelarUber(){
+
   }
 
   @override
@@ -218,24 +284,31 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
     super.initState();
     _recuperaUltimaLocalizacaoConhecida();
     _adicionarListenerLocalizacao();
+
     _statusUberNaoChamado();
+
   }
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Painel passageiro"),
         actions: <Widget>[
           PopupMenuButton<String>(
             onSelected: _escolhaMenuItem,
-            itemBuilder: (context) {
-              return itensMenu.map((String item) {
+            itemBuilder: (context){
+
+              return itensMenu.map((String item){
+
                 return PopupMenuItem<String>(
                   value: item,
                   child: Text(item),
                 );
+
               }).toList();
+
             },
           )
         ],
@@ -256,6 +329,7 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
               visible: _exibirCaixaEnderecoDestino,
               child: Stack(
                 children: <Widget>[
+
                   Positioned(
                     top: 0,
                     left: 0,
@@ -268,7 +342,8 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
                         decoration: BoxDecoration(
                             border: Border.all(color: Colors.grey),
                             borderRadius: BorderRadius.circular(3),
-                            color: Colors.white),
+                            color: Colors.white
+                        ),
                         child: TextField(
                           readOnly: true,
                           decoration: InputDecoration(
@@ -276,15 +351,12 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
                                 margin: EdgeInsets.only(left: 20),
                                 width: 10,
                                 height: 10,
-                                child: Icon(
-                                  Icons.location_on,
-                                  color: Colors.green,
-                                ),
+                                child: Icon(Icons.location_on, color: Colors.green,),
                               ),
                               hintText: "Meu local",
                               border: InputBorder.none,
-                              contentPadding:
-                                  EdgeInsets.only(left: 15, top: 16)),
+                              contentPadding: EdgeInsets.only(left: 15, top: 16)
+                          ),
                         ),
                       ),
                     ),
@@ -301,7 +373,8 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
                         decoration: BoxDecoration(
                             border: Border.all(color: Colors.grey),
                             borderRadius: BorderRadius.circular(3),
-                            color: Colors.white),
+                            color: Colors.white
+                        ),
                         child: TextField(
                           controller: _controllerDestino,
                           decoration: InputDecoration(
@@ -309,19 +382,16 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
                                 margin: EdgeInsets.only(left: 20),
                                 width: 10,
                                 height: 10,
-                                child: Icon(
-                                  Icons.local_taxi,
-                                  color: Colors.black,
-                                ),
+                                child: Icon(Icons.local_taxi, color: Colors.black,),
                               ),
                               hintText: "Digite o destino",
                               border: InputBorder.none,
-                              contentPadding:
-                                  EdgeInsets.only(left: 15, top: 16)),
+                              contentPadding: EdgeInsets.only(left: 15, top: 16)
+                          ),
                         ),
                       ),
                     ),
-                  ),
+                  )
                 ],
               ),
             ),
@@ -338,9 +408,10 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
                       _textoBotao,
                       style: TextStyle(color: Colors.white, fontSize: 20),
                     ),
-                    color: _colorBotao,
+                    color: _corBotao,
                     padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                    onPressed: _funcaoBotao),
+                    onPressed: _funcaoBotao
+                ),
               ),
             )
           ],
